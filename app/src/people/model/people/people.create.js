37ac577c6de1630ab51f1512704adf0042a196ba
jrbigmon <juniorterriaga0@gmail.com>
2024-04-02 23:13:10 -0300

    fix: insert people

--- app/src/people/model/people/people.create.js
+++ app/src/people/model/people/people.create.js
@@ -2,19 +2,17 @@ const objectToArrayOfValues = require("../../../utils/object-to-array-of-values"
 
 function PeopleCreate(connection) {
   this.exec = async function (people) {
-    const sql = "INSERT INTO peoples (id, name) VALUES ?";
+    const sql = "INSERT INTO peoples (id, name) VALUES (?)";
 
     const values = objectToArrayOfValues(people.toJSON());
 
-    const id = await new Promise((resolve) => {
+    await new Promise((resolve) => {
       connection.query(sql, [values], function (err, result) {
         if (err) throw err;
 
-        resolve(result?.insertId);
+        resolve(result);
       });
     });
-
-    return id;
   };
 }
 
