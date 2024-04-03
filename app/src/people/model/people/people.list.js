965124e8f32fbeff033c023dd938805b679907e6
jrbigmon <juniorterriaga0@gmail.com>
2024-04-02 21:13:36 -0300

    style: change application locale

---
+++ app/src/people/model/people/people.list.js
@@ -0,0 +1,19 @@
+const People = require("../../repository/people");
+
+function PeopleList(connection) {
+  this.exec = async function () {
+    const sql = `SELECT * FROM peoples`;
+
+    const peoples = await new Promise((resolve) => {
+      connection.query(sql, function (err, results) {
+        if (err) throw err;
+
+        resolve(JSON.stringify(JSON.parse(results)));
+      });
+    });
+
+    return peoples?.map((people) => new People(people?.name, people?.id));
+  };
+}
+
+module.exports = PeopleList;
