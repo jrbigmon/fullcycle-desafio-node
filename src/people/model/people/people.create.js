bcb2137c8fcf10437dc70d7fe317211775ba2d2d
jrbigmon <juniorterriaga0@gmail.com>
2024-04-02 21:09:54 -0300

    feat: create people function

---
+++ src/people/model/people/people.create.js
@@ -0,0 +1,21 @@
+const objectToArrayOfValues = require("../../../utils/object-to-array-of-values");
+
+function PeopleCreate(connection) {
+  this.exec = async function (people) {
+    const sql = "INSERT INTO peoples (id, name) VALUES ?";
+
+    const values = objectToArrayOfValues(people.toJSON());
+
+    const id = await new Promise((resolve) => {
+      connection.query(sql, [values], function (err, result) {
+        if (err) throw err;
+
+        resolve(result?.insertId);
+      });
+    });
+
+    return id;
+  };
+}
+
+module.exports = PeopleCreate;
